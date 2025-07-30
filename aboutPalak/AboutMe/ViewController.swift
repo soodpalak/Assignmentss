e1c9215ae32f457b546e2accd74a666d325376b1
soodpalak <palak2252.be23@chitkara.edu.in>
2025-07-30 11:09:38 +0530

    commit

---
+++ aboutPalak/AboutMe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AboutMe
+//
+//  Created by Student on 29/07/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ aboutPalak/AboutMeTests/AboutMeTests.swift
@@ -0,0 +1,17 @@
+//
+//  AboutMeTests.swift
+//  AboutMeTests
+//
+//  Created by Student on 29/07/25.
+//
+
+import Testing
+@testable import AboutMe
+
+struct AboutMeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
